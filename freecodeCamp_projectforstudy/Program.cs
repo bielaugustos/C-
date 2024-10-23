7e8d1a17ddc0d23d15552ecac9e3834b64003b11
Gabriel Augusto <gabrielaugusto@mac.local>
2024-10-23 18:12:41 -0300

    (module I) types of data

---
+++ freecodeCamp_projectforstudy/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            // int x = 10, y = 20, z = 30; 
+
+            // Best practice to declare variables
+            // int x = 10;
+            // int y = 20;
+            // int z = 30;
+
+            // TYPES OF VALUES
+
+            int age = 23;
+            System.Console.WriteLine(age);
+            System.Console.WriteLine(int.MaxValue);
+            System.Console.WriteLine(int.MinValue);
+
+            long bignumber = 9000000L;
+            Console.WriteLine(bignumber);
+            System.Console.WriteLine(long.MaxValue);
+            System.Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            System.Console.WriteLine(negative);
+            System.Console.WriteLine(double.MaxValue);
+            System.Console.WriteLine(double.MinValue);
+
+            float precision = 5.000001F;
+            System.Console.WriteLine(precision);
+            System.Console.WriteLine(float.MaxValue);
+            System.Console.WriteLine(float.MinValue);
+
+            decimal money = 14.99M;
+            System.Console.WriteLine(money);
+            System.Console.WriteLine(decimal.MaxValue);
+            System.Console.WriteLine(decimal.MinValue);
+
+            // Determines console to wait the user before run the code
+            Console.ReadLine();
+        }
+    }
+}
+
+
